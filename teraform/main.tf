523869dfcbfc27ff7912b7ed2bb1b29ad73b352f
Godfrey2004 <godfreyleander641@gmail.com>
2025-10-30 22:03:51 +0530

    Complete DevOps pipeline with Jenkins, Docker, Ansible, Terraform

--- teraform/main.tf
+++ teraform/main.tf
@@ -1,5 +1,20 @@
-resource "null_resource" "deploy_app" {
+# Terraform configuration for WebTech App deployment
+resource "null_resource" "deploy_webtech_app" {
   provisioner "local-exec" {
     command = "docker-compose up -d"
   }
+  
+  provisioner "local-exec" {
+    when    = destroy
+    command = "docker-compose down"
+  }
+}
+
+# Output the application URL
+output "application_url" {
+  value = "http://localhost:3019"
+}
+
+output "jenkins_url" {
+  value = "http://localhost:8080"
 }
